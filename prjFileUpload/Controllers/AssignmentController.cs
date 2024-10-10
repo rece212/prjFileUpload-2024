afd618f9c18e60666ce3cffe755c650292e3ce9b
rece212 <rwanvig@varsitycollege.co.za>
2024-10-10 14:41:19 +0200

    Add project files.

---
+++ prjFileUpload/Controllers/AssignmentController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using prjFileUpload.Models;
+
+namespace prjFileUpload.Controllers
+{
+    public class AssignmentController : Controller
+    {
+        private readonly IWebHostEnvironment _environment;
+        public AssignmentController(IWebHostEnvironment environment)
+        {
+            _environment = environment;
+        }
+        private static List<Assignment> assignments = new List<Assignment>();
+        public IActionResult Index()
+        {
+            return View(assignments);
+        }
+        [HttpPost]
+        public IActionResult Upload(IFormFile file, string uploaderName)
+        {
+            if (file != null && file.Length > 0)
+            {
+                var fileName = Path.GetFileName(file.FileName);
+                var path = Path.Combine(_environment.WebRootPath, "uploads", fileName);
+                using (var stream = new FileStream(path, FileMode.Create))
+                {
+                    file.CopyTo(stream);
+                }
+                assignments.Add(new Assignment
+                {
+                    Id = assignments.Count + 1,
+                    FileName = fileName,
+                    UploaderName = uploaderName,
+                    UploadDate = DateTime.Now
+                });
+            }
+            return RedirectToAction("Index");
+        }
+        public IActionResult OpenFile(string fileName)
+        {
+            var path = Path.Combine(_environment.WebRootPath, "uploads", fileName);
+            var fileBytes =System.IO.File.ReadAllBytes(path);
+            return File(fileBytes, "application/octet-stream", fileName);
+        }
+    }
+}
---
+++ prjFileUpload/Models/Assignment.cs
@@ -0,0 +1,10 @@
+﻿namespace prjFileUpload.Models
+{
+    public class Assignment
+    {
+        public int Id { get; set; }
+        public string FileName { get; set; }
+        public string UploaderName { get; set; }
+        public DateTime UploadDate { get; set; }
+    }
+}
